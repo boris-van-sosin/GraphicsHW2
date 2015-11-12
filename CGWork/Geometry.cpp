e6132d86911d591a6c5d2e7ed913eaa2d1e29158
boris-van-sosin <sosin@LAP383.cs.technion.ac.il>
2015-11-12 19:58:51 +0200

    Added classes for line segment, polygon, polygonal object
    They have some basic methods, but by far not finished,
    Missing: bounding boxes, colors, etc.

--- CGWork/Geometry.cpp
+++ CGWork/Geometry.cpp
@@ -16,7 +16,12 @@ Point3D::Point3D(const Point3D& other)
 {
 }
 
-Point3D Point3D::operator + (const Point3D& other) const
+Point3D::Point3D(const HomogeneousPoint& h)
+	: Point3D(h.x / h.w, h.y / h.w, h.z / h.w)
+{
+}
+
+Point3D Point3D::operator+(const Point3D& other) const
 {
 	return Point3D(x + other.x, y + other.y, z + other.z);
 }
@@ -71,4 +76,73 @@ Point3D Point3D::Normalized() const
 Point3D operator*(double s, const Point3D& p)
 {
 	return Point3D(p.x*s, p.y*s, p.z*s);
+}
+
+const Point3D Point3D::Zero;
+
+HomogeneousPoint::HomogeneousPoint()
+	: HomogeneousPoint(0, 0, 0, 1)
+{
+}
+
+HomogeneousPoint::HomogeneousPoint(double x_, double y_, double z_, double w_)
+	: HomogeneousPoint(x, y, z, w)
+{
+}
+
+HomogeneousPoint::HomogeneousPoint(const HomogeneousPoint& other)
+	: HomogeneousPoint(other.x, other.y, other.z, 1)
+{
+}
+
+HomogeneousPoint::HomogeneousPoint(const Point3D& p)
+	: HomogeneousPoint(p.x, p.y, p.z, 1)
+{
+}
+
+double HomogeneousPoint::operator*(const HomogeneousPoint& other) const
+{
+	return x*other.x + y*other.y + z*other.z + w*other.w;
+}
+
+LineSegment::LineSegment(const Point3D& p0_, const Point3D& p1_)
+	: p0(p0_), p1(p1_)
+{
+}
+
+Polygon::Polygon(const std::vector<Point3D>& points_)
+	: points(points_)
+{
+}
+
+std::vector<LineSegment> Polygon::Edges() const
+{
+	std::vector<LineSegment> res;
+	res.reserve(points.size());
+	for (int i = 0; i < points.size(); ++i)
+	{
+		res.push_back(LineSegment(points[i], points[(i + 1) % points.size()]));
+	}
+	return res;
+}
+
+Vector3D Polygon::Normal() const
+{
+	for (int i = 0; i < points.size(); ++i)
+	{
+		const Point3D& p0 = points[i];
+		const Point3D& p1 = points[(i + 1) % points.size()];
+		Vector3D n = p0.Cross(p1); // with the point order
+		if (n.SquareNorm() > GEOMETRIC_COMPUTATION_ESPILON)
+		{
+			return n;
+		}
+	}
+	// polygon is a "flat" line
+	return Point3D::Zero;
+}
+
+PolygonalObject::PolygonalObject(const std::vector<PolygonalObject>& polygons_)
+	: polygons(polygons_)
+{
 }
--- CGWork/Geometry.h
+++ CGWork/Geometry.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <vector>
+
+#define GEOMETRIC_COMPUTATION_ESPILON 1e-10
+
+class HomogeneousPoint;
+
 class Point3D
 {
 public:
 	Point3D();
 	Point3D(double x_, double y_, double z_);
 	Point3D(const Point3D& other);
+	explicit Point3D(const HomogeneousPoint& h);
 
 public:
 	Point3D operator+(const Point3D& other) const;
@@ -22,6 +29,56 @@ public:
 
 public:
 	double x, y, z;
+	static const Point3D Zero;
 };
 
 Point3D operator*(double s, const Point3D& p);
+
+typedef Point3D Vector3D;
+
+class HomogeneousPoint
+{
+public:
+	HomogeneousPoint();
+	HomogeneousPoint(double x_, double y_, double z_, double w_=1.0);
+	HomogeneousPoint(const HomogeneousPoint& other);
+	explicit HomogeneousPoint(const Point3D& p);
+
+	double operator*(const HomogeneousPoint& other) const;
+
+public:
+	double x, y, z, w;
+};
+
+class LineSegment
+{
+public:
+	LineSegment(const Point3D& p0_, const Point3D& p1_);
+
+	//int Octant() const;
+	/*
+	321
+	4*0
+	567
+	*/
+
+	Point3D p0, p1;
+};
+
+class Polygon
+{
+public:
+	Polygon(const std::vector<Point3D>& points_);
+
+	std::vector<LineSegment> Edges() const;
+	Vector3D Normal() const;
+
+	std::vector<Point3D> points;
+};
+
+class PolygonalObject
+{
+	PolygonalObject(const std::vector<PolygonalObject>& polygons_);
+
+	std::vector<PolygonalObject> polygons;
+};
