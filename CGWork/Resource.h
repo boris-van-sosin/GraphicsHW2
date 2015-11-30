6e9b7744744be613790576548f5e507f8d8219e7
boris-van-sosin <sosin@LAP383.cs.technion.ac.il>
2015-11-30 16:53:22 +0200

    Drew icons for normals
    they actually look nice...

--- CGWork/Resource.h
+++ CGWork/Resource.h
@@ -52,13 +52,15 @@
 #define ID_BUTTON_SUBDIVIDE             32797
 #define ID_OPTIONS_PERSPECTIVECONTROL   32798
 #define ID_OPTIONS_MOUSESENSITIVITY     32799
+#define ID_BUTTON32801                  32801
+#define ID_BUTTON32802                  32802
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        139
-#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_COMMAND_VALUE         32803
 #define _APS_NEXT_CONTROL_VALUE         1040
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
